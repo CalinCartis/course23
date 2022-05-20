772f413d4bbdee2ce76494f995e705cf00cc40db
CalinCartis <calin_cartis@yahoo.com>
2022-05-20 11:46:45 +0300

    Filter Page

--- src/components/ClientComponent.vue
+++ src/components/ClientComponent.vue
@@ -8,16 +8,19 @@ const props = defineProps({
 
 <template>
   <div class="card shadow-lg p-3 mb-5 bg-body rounded-custom text-center">
-    <p><strong>Name</strong></p>
-    <p>Email</p>
-    <p>Phone</p>
+    <p>
+      <i class="bi bi-person me-1"></i> <strong>{{ props.name }}</strong>
+    </p>
+    <p><i class="bi bi-envelope me-2"></i>{{ props.email }}</p>
+    <p><i class="bi bi-telephone me-2"></i>{{ props.phone }}</p>
   </div>
 </template>
 
 <style scoped>
 .card {
-  max-width: 300px;
+  font-size: 0.9rem;
 }
+
 .rounded-custom {
   border-radius: 25px;
   border-color: transparent;
--- src/views/FilterView.vue
+++ src/views/FilterView.vue
@@ -1,13 +1,92 @@
 <script setup>
 import ClientComponent from "@/components/ClientComponent.vue";
+import { ref, onMounted } from "vue";
+
+const persons = [
+  {
+    name: "Calin Cartis",
+    email: "calin.cartis@emanuel.ro",
+    phone: "077400000000",
+  },
+  {
+    name: "Daniel Oanta",
+    email: "daniel.oanta@emanuel.ro",
+    phone: "077400000000",
+  },
+  {
+    name: "Nathan van Velden",
+    email: "nathan.vanvelden@emanuel.ro",
+    phone: "077400000000",
+  },
+  {
+    name: "David van Velden",
+    email: "david.vanvelden@emanuel.ro",
+    phone: "077400000000",
+  },
+  {
+    name: "Marcel Rural",
+    email: "marcel.rural@emanuel.ro",
+    phone: "077400000000",
+  },
+];
+
+var letters = ref([]);
+
+for (let i = 0; i < persons.length; i++) {
+  if (!letters.value.includes(persons[i].name.charAt(0)))
+    letters.value.push(persons[i].name.charAt(0));
+}
+
+var filter = ref();
+function filterByLetter(newLetter) {
+  filter.value = persons.filter((person) => person.name.startsWith(newLetter));
+}
 </script>
 
 <template>
   <main>
     <div class="container-fluid">
+      <div class="row mt-5">
+        <div class="col" v-for="person in persons">
+          <ClientComponent
+            :name="person.name"
+            :email="person.email"
+            :phone="person.phone"
+          ></ClientComponent>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
       <div class="row">
-        <div class="col">
-          <ClientComponent></ClientComponent>
+        <div class="col center-block">
+          <p>Alege o litera pentru a filtra:</p>
+          <div class="btn-group" role="group" v-for="letter in letters">
+            <input
+              type="radio"
+              class="btn-check"
+              name="options-outlined"
+              :id="letter"
+              autocomplete="off"
+              checked
+            />
+            <label :for="letter"
+              ><button
+                class="btn btn-outline-primary me-1"
+                @click="filterByLetter(letter)"
+              >
+                {{ letter }}
+              </button></label
+            >
+          </div>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col" v-for="person in filter">
+          <ClientComponent
+            :name="person.name"
+            :email="person.email"
+            :phone="person.phone"
+          ></ClientComponent>
         </div>
       </div>
     </div>
