efc9bb21c8269b05f2e7579b1977b908c0532c05
CalinCartis <calin_cartis@yahoo.com>
2022-05-22 17:20:08 +0300

    Added style, loading text, interval to refresh prices

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <script setup>
-import PlanComponent from "@/components/PlanComponent.vue";
 import axios from "axios";
 import { ref, onMounted } from "vue";
 
@@ -7,7 +6,15 @@ var bpi = ref();
 onMounted(() => {
   axios.get("https://api.coindesk.com/v1/bpi/currentprice.json").then((res) => {
     bpi.value = res.data.bpi;
+    document.getElementById("loading").remove();
   });
+  setInterval(() => {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then((res) => {
+        bpi.value = res.data.bpi;
+      });
+  }, 25000);
 });
 </script>
 
@@ -15,15 +22,32 @@ onMounted(() => {
   <main>
     <div class="container-fluid">
       <div class="row">
-        <div class="col">
-          <h3>BPI:</h3>
-          <ul v-for="item in bpi">
-            <li>{{ item.code }} ({{ item.description }}) {{ item.rate }}</li>
-          </ul>
+        <h3 class="mb-4">
+          <i class="bi bi-currency-bitcoin"></i> Bitcoin Price Index:
+          <div id="loading" class="col text-center">Loading...</div>
+        </h3>
+        <div class="col" v-for="item in bpi">
+          <div
+            class="card shadow-lg p-3 mb-5 bg-body rounded-custom text-center"
+          >
+            <p>
+              <strong>{{ item.code }}</strong> ({{ item.description }})
+            </p>
+            <p><i class="bi bi-cash me-2"></i>{{ item.rate }}</p>
+          </div>
         </div>
       </div>
     </div>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.card {
+  font-size: 0.9rem;
+}
+
+.rounded-custom {
+  border-radius: 25px;
+  border-color: transparent;
+}
+</style>
